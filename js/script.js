4d5ee572156dd3becc692dfe6d7ad4c82ee64519
Marielle K <marielle.koffi1@gmail.com>
2022-06-10 17:05:30 +0200

    test2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+var myName = "Marielle";
+var salutation = "Bonjour" + " " + myName;
+console.log(salutation);
 
+myName = prompt ("Quel est ton prénom?");
+console.log(myName);
+
+document.body.innerHTML += "<h2>Bonjour " + myName + "!</h2>";
